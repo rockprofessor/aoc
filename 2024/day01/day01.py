0a852e8402f8e2a430e1f6df0e45f461b7764136
rockprofessor <robertmachovec@gmx.at>
2024-12-03 17:16:01 +0100

    minor changes

--- 2024/day01/day01.py
+++ 2024/day01/day01.py
@@ -10,12 +10,12 @@ for line in data:
 left.sort()
 right.sort()
 
-#ans1 = 0
+ans1 = 0
 ans2 = 0
 
 for i in range(len(left)):
-    #ans1 += abs(left[i] - right[i])
+    ans1 += abs(left[i] - right[i])
     ans2 += left[i] * right.count(left[i])
 
-#print('Answer 1:', ans1)
+print('Answer 1:', ans1)
 print('Answer 2:', ans2)
