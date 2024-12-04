bd5e897593105be8e782383a9b23bce001ac2684
rockprofessor <robertmachovec@gmx.at>
2024-12-04 07:19:39 +0100

    minor changes

--- 2024/day04/day04.py
+++ 2024/day04/day04.py
@@ -2,16 +2,17 @@ data = [i.strip() for i in open('4.in')]
     
 ans1 = 0    
 ans2 = 0
-for l in range(len(data)):
-    for c in range(len(data[0])):
+r = len(data)
+for l in range(r):
+    for c in range(r):
         if data[l][c] == 'X':
             for dl, dc in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1),(-1, 1), (-1, -1)]:
-                if 0 <= l + 3*dl < len(data) and 0 <= c + 3*dc < len(data[0]):
+                if 0 <= l + 3 * dl < r and 0 <= c + 3 * dc < r:
                     if data[l+dl][c+dc] + data[l+2*dl][c+2*dc] + data[l+3*dl][c+3*dc] == 'MAS':
                         ans1 += 1
 
         if data[l][c] == 'A':
-            if 0 < l < (len(data) - 1) and 0 < c < (len(data[0]) - 1):
+            if 0 < l < (r - 1) and 0 < c < (r - 1):
                 cross = data[l-1][c-1] + data[l+1][c-1] + data[l-1][c+1] + data[l+1][c+1]
                 if cross in ['MMSS', 'SSMM', 'SMSM', 'MSMS']:
                     ans2 += 1
