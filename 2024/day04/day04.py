ef4ec1df5c1d0a632200141fb59917d6d8489e6b
rockprofessor <robertmachovec@gmx.at>
2024-12-04 18:21:29 +0100

    optimized

--- 2024/day04/day04.py
+++ 2024/day04/day04.py
@@ -1,20 +1,18 @@
-data = [i.strip() for i in open('4.in')]
+d = [i.strip() for i in open('4.in')]
 
 ans1 = 0
 ans2 = 0
-r = len(data)
+r = len(d)
 for l in range(r):
     for c in range(r):
-        if data[l][c] == 'X':
+        if d[l][c] == 'X':
             for dl, dc in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1),(-1, 1), (-1, -1)]:
-                if 0 <= l + 3 * dl < r and 0 <= c + 3 * dc < r:
-                    if data[l+dl][c+dc] + data[l+2*dl][c+2*dc] + data[l+3*dl][c+3*dc] == 'MAS':
+                if 0 <= l+3*dl < r and 0 <= c+3*dc < r and d[l+dl][c+dc]+d[l+2*dl][c+2*dc]+d[l+3*dl][c+3*dc] == 'MAS':
                         ans1 += 1
 
-        if data[l][c] == 'A':
+        if d[l][c] == 'A':
             if 0 < l < (r - 1) and 0 < c < (r - 1):
-                cross = data[l-1][c-1] + data[l+1][c-1] + data[l-1][c+1] + data[l+1][c+1]
-                if cross in ['MMSS', 'SSMM', 'SMSM', 'MSMS']:
+                if d[l-1][c-1] + d[l+1][c-1] + d[l-1][c+1] + d[l+1][c+1] in ['MMSS', 'SSMM', 'SMSM', 'MSMS']:
                     ans2 += 1
 
 print('Answer 1:', ans1)
