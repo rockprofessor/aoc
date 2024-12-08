a7133f292216660701e7a593de031294ae05de7b
rockprofessor <robertmachovec@gmx.at>
2024-12-08 08:10:55 +0100

    optimized

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -1,13 +1,9 @@
 data = [i.strip() for i in open('8.in')]
+r = len(data)
 
-def dist(a, b):
-    return (b[0] - a[0], b[1] - a[1])
-
-def addit(a, b):
-    return (a[0] + b[0], a[1] + b[1])
-
-def mul(n, a):
-    return (n * a[0], n * a[1])
+def dist(a, b): return (b[0] - a[0], b[1] - a[1])
+def addit(a, b): return (a[0] + b[0], a[1] + b[1])
+def mul(n, a): return (n * a[0], n * a[1])
 
 grid = {}
 ant = {}
@@ -19,21 +15,20 @@ for l, line in enumerate(data):
                 ant[desc] = []
             ant[desc].append((c, l))
 
-r = len(data)
-an1 = set() 
-an2 = set() 
+an1 = set()
+an2 = set()
+
 for z in ant:
     for i in range(len(ant[z]) - 1):
         for j in range(i + 1, len(ant[z])):
             a, b = ant[z][i], ant[z][j]
             cand = [addit(b, dist(a, b)), addit(a, dist(b, a))]
-            for c in cand:
-                if 0 <= c[0] < r and 0 <= c[1] < r:
-                    an1.add(c)
             for m in range(r):
                 cand = [addit(b, mul(m, dist(a, b))), addit(a, mul(m, dist(b, a)))]
                 for c in cand:
                     if 0 <= c[0] < r and 0 <= c[1] < r:
+                        if m == 1:
+                            an1.add(c)
                         an2.add(c)
 
 print('Answer 1:', len(an1))
