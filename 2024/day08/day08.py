9b9c55334e9441bac4905087e305afb94ac28643
rockprofessor <robertmachovec@gmx.at>
2024-12-08 08:00:00 +0100

    optimized

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -1,14 +1,7 @@
-import math
 data = [i.strip() for i in open('8.in')]
 
 def dist(a, b):
     return (b[0] - a[0], b[1] - a[1])
-
-def dist2(a, b):
-    dx = b[0] - a[0]
-    dy = b[1] - a[1]
-    t = math.gcd(dx, dy)
-    return (dx / t, dy / t)
 
 def addit(a, b):
     return (a[0] + b[0], a[1] + b[1])
@@ -17,21 +10,18 @@ def mul(n, a):
     return (n * a[0], n * a[1])
 
 grid = {}
+ant = {}
 for l, line in enumerate(data):
     for c, desc in enumerate(line):
         grid[c, l] = desc
-
-ant = {}
-for spot in grid:
-    if grid[spot] != '.':
-        if grid[spot] not in ant:
-            ant[grid[spot]] = [spot]
-        else:
-            ant[grid[spot]].append(spot)
+        if desc != '.':
+            if desc not in ant:
+                ant[desc] = []
+            ant[desc].append((c, l))
 
 r = len(data)
-an1 = []
-an2 = []
+an1 = set() 
+an2 = set() 
 for z in ant:
     for i in range(len(ant[z]) - 1):
         for j in range(i + 1, len(ant[z])):
@@ -39,12 +29,12 @@ for z in ant:
             cand = [addit(b, dist(a, b)), addit(a, dist(b, a))]
             for c in cand:
                 if 0 <= c[0] < r and 0 <= c[1] < r:
-                    an1.append(c)
+                    an1.add(c)
             for m in range(r):
-                cand = [addit(b, mul(m, dist2(a, b))), addit(a, mul(m, dist2(b, a)))]
+                cand = [addit(b, mul(m, dist(a, b))), addit(a, mul(m, dist(b, a)))]
                 for c in cand:
                     if 0 <= c[0] < r and 0 <= c[1] < r:
-                        an2.append(c)
+                        an2.add(c)
 
-print('Answer 1:', len(set(an1)))
-print('Answer 2:', len(set(an2)))
+print('Answer 1:', len(an1))
+print('Answer 2:', len(an2))
