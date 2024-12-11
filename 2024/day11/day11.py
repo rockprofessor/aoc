6776883081f9a2ccd8b0eac1ed2879d6bb959d2e
rockprofessor <robertmachovec@gmx.at>
2024-12-11 16:45:49 +0100

    optimized

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -4,16 +4,15 @@ stones = open('11.in').read().strip()
 stones = [int(i) for i in stones.split()]
 
 @lru_cache(maxsize=None)
-def cut(s, g, b):
-    g += 1
-    if g > b:  
+def cut(s, b):
+    if b == 0:  
         return 1
-    if s == 0: return cut(1, g, b)
+    if s == 0: return cut(1, b - 1)
     if len(str(s)) % 2 == 0:
         left = int(str(s)[:len(str(s))//2])
         right = int(str(s)[len(str(s))//2:])
-        return cut(left, g, b) +  cut(right, g, b)
-    return cut(s * 2024, g, b)
+        return cut(left, b - 1) +  cut(right, b - 1)
+    return cut(s * 2024, b - 1)
 
-print('Answer 1:', sum(cut(s, 0, 25) for s in stones))
-print('Answer 2:', sum(cut(s, 0, 75) for s in stones))
+print('Answer 1:', sum(cut(s, 25) for s in stones))
+print('Answer 2:', sum(cut(s, 75) for s in stones))
