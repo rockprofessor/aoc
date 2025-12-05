3f592088c5286a2abaef4cfecacb32a5c2df43d0
rockprofessor <robertmachovec@gmx.at>
2025-12-05 20:55:30 +0100

    updated

--- 2025/day05/day05.py
+++ 2025/day05/day05.py
@@ -11,18 +11,19 @@ for i in ids:
             fresh = True
             c1 += 1
             break
+
 print('Answer 1:', c1)
 
 c2 = 0
-ranges = sorted(ranges, key=lambda x: x[0])
-
+ranges = sorted(ranges, key = lambda x: x[0])
 ref = 0
+
 while ranges: 
     i = max(ref, ranges[0][0])
-    R = ranges[0]
-    if i <= R[1]:
-        c2 += R[1] + 1 - i
-        ref = R[1] + 1
+    r = ranges[0]
+    if i <= r[1]:
+        c2 += r[1] + 1 - i
+        ref = r[1] + 1
     ranges.pop(0)
 
 print('Answer 2:', c2)
