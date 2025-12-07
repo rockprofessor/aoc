db411084a95b49d2e29417aab723f8ab580b2f29
rockprofessor <robertmachovec@gmx.at>
2025-12-07 18:06:20 +0100

    updated

--- 2025/day07/day07.py
+++ 2025/day07/day07.py
@@ -1,13 +1,11 @@
-# (col, row)
 grid = {(x,y): c for y, row in enumerate(open('7.in')) for x, c in enumerate(row)}
 for field in grid:
     if grid[field] == 'S':
         start = field
 
-R = (max(grid.keys(), key=lambda x: x[1]))[1]
+R = max(grid.keys())[0]
 beam = [start]
 c1 = 0
-c2 = 0
 
 i = 0
 while i < R:
@@ -22,8 +20,6 @@ while i < R:
                 c1 += 1
             else:
                 newbeam.append(look)
-        else:
-            go = False
 
     for n in set(newbeam):
         beam.append(n)
@@ -39,27 +35,24 @@ while i < R:
     while beam2:
         look, numb = beam2.popitem()
         look = (look[0], look[1] + 1)
-        if look in grid:
-            if grid[look] == '^':
-                if (look[0] - 1, look[1]) in newbeam:
-                    newbeam[(look[0] - 1, look[1])] += numb
-                else:
-                    newbeam[(look[0] - 1, look[1])] = numb
-                if (look[0] + 1, look[1]) in newbeam:
-                    newbeam[(look[0] + 1, look[1])] += numb
-                else:
-                    newbeam[(look[0] + 1, look[1])] = numb
+        if grid[look] == '^':
+            if (look[0] - 1, look[1]) in newbeam:
+                newbeam[(look[0] - 1, look[1])] += numb
             else:
-                if look in newbeam:
-                    newbeam[look] += numb
-                else:
-                    newbeam[look] = numb
+                newbeam[(look[0] - 1, look[1])] = numb
+
+            if (look[0] + 1, look[1]) in newbeam:
+                newbeam[(look[0] + 1, look[1])] += numb
+            else:
+                newbeam[(look[0] + 1, look[1])] = numb
+        else:
+            if look in newbeam:
+                newbeam[look] += numb
+            else:
+                newbeam[look] = numb
+
     for n in newbeam:
         beam2[n] = newbeam[n]
     i += 1
 
-c2 = 0
-for k in newbeam:
-    c2 += newbeam[k]
-
-print('Answer 2:', c2)
+print('Answer 2:', sum(newbeam.values()))
