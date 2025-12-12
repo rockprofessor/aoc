6288e8a0d3901817bd0407267c399269342f4a63
rockprofessor <robertmachovec@gmx.at>
2025-12-12 15:40:22 +0100

    updated

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -8,7 +8,31 @@ for line in data:
 def go(pos):
     if pos == 'out':
         return 1
-    return sum([go(x) for x in graph[pos]])
+    s = 0
+    for x in graph[pos]:
+        s += go(x)
+    return s
 
 print('Answer 1:', go('you'))
 
+
+#svr -> out
+
+
+def go2(pos, d, f):
+    if pos == 'dac':
+        a = 1
+    elif pos == 'fft':
+        b = 1
+
+    elif pos == 'out' and d == 1 and f == 1:
+        return 1
+
+    elif pos == 'out' and (d == 0 or f == 0):
+        return 0
+
+    s = 0
+    for x in graph[pos]:
+        s += go2(x, a, b)
+
+print('Answer 2:', go2('svr', 0, 0))
